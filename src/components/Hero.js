cee47fda774d6e2b4893b2ce439b5ca454a50baf
giorgi <giorgi.doreuli3721@gmail.com>
2022-01-24 05:12:08 +0400

    react-website

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import { Link} from "react-scroll";
 function Hero() {
   return (
     <div className='hero-container hero' id='hero'>
-      <img src='kazbegi.jpg' alt = 'cover' class = 'main-photo'/>
+      <img src='main-photo.jpg' alt = 'cover' class = 'main-photo'/>
       <h1>GEORGIA AWAITS</h1>
       <p>What are you waiting for?</p>
         <Link to='places' className='btn'>
